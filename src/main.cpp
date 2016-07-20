5041944db4a8f229122e107e45718e3449d8c721
Damon Kelly <damonkelly@gmail.com>
2016-07-20 18:07:15 -0400

    Remove unused code

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
-
 #include <iostream>
 #include <string>
 #include <fstream>
 
-#include <Eigen/Geometry>
 #include <cpd/nonrigid.hpp>
 #include <cpd/affine.hpp>
 #include <cpd/matrix.hpp>
 
 using namespace std;
+
 
 int
 main (int argc, char** argv)
@@ -18,20 +17,15 @@ main (int argc, char** argv)
     // set data directory
     string data_dir = "/home/damon/gcubed/repos/cpdexample/";
 
-    // load matrix 1
-    cout << "loading matrix 1" << endl;
-    //string fname1 = data_dir + "fish.txt";
+    // load contour 1
+    cout << "loading contour 1" << endl;
     string fname1 = data_dir + "contour1.txt";
     cpd::Matrix mat1 = cpd::matrix_from_path(fname1);
-    cout << "mat1" << endl << mat1 << endl;
 
-    // load matrix 2
-    cout << "loading matrix 2" << endl;
+    // load contour 2
+    cout << "loading contour 2" << endl;
     string fname2 = data_dir + "contour2.txt";
     cpd::Matrix mat2 = cpd::matrix_from_path(fname2);
-    //Eigen::Rotation2D<double> rotation(M_PI / 6);
-    //Eigen::RowVector2d translation(1, 2);
-    //cpd::Matrix mat2((mat1 + translation.replicate(mat1.rows(),1)) * rotation.matrix());
 
     // perform registration
     //auto result = cpd::nonrigid(mat1, mat2);
@@ -41,10 +35,4 @@ main (int argc, char** argv)
     ofstream result_file(data_dir + "result.txt");
     result_file << result.points;
     result_file.close();
-
-
-    //cout << "mat2" << endl << mat2 << endl;
-    //cout << "result" << endl << result.points << endl;
-
-
 }
