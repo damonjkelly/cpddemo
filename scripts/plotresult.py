54559531a91766d04a4b083c7654ca3803c8709f
Damon Kelly <damonkelly@gmail.com>
2016-07-20 20:21:52 -0400

    Animate plots

--- scripts/plotresult.py
+++ scripts/plotresult.py
@@ -7,24 +7,57 @@ Created on Mon Jul 18 20:37:47 2016
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib.animation as animation
 
 # set data directory
-data_dir = "/home/damon/gcubed/repos/cpdexample/"
+data_dir = "/home/damon/gcubed/repos/cpdexample/data/"
 
-# load contour 1
+# load contour 1 and 2
 c1 = np.loadtxt(data_dir + "contour1.txt")
-
-# load contour 2
 c2 = np.loadtxt(data_dir + "contour2.txt")
 
-# load result
-result = np.loadtxt(data_dir + "result.txt")
 
-# plot contours
-plt.figure()
+# Setup up the figure, axis and plot elements to animate
+fig, ax = plt.subplots()
+plt.title("affine cpd")
 plt.gca().invert_yaxis()
-plt.plot(c1[:,0], c1[:,1], 'b-')
-plt.plot(c2[:,0], c2[:,1], 'ro')
-plt.plot(result[:,0], result[:,1], 'go')
+line1, = ax.plot(c1[:,0], c1[:,1], 'b+')    # fixed target
+line2, = ax.plot([], [], 'go')              # src points
+
+
+# Initialization function plots the background of each frame
+def init():
+    line1.set_data(c1[:,0], c1[:,1])    # fixed target
+    return line1, line2, line3
+
+# Animation function which is called sequentially
+def animate(i):
+    print(i)
+    if i == 0:
+        line2.set_data(c2[:,0], c2[:,1])
+    else:
+        result = np.loadtxt(data_dir + "affine" + str(i) + ".txt")
+        line2.set_data(result[:,0], result[:,1])
+    return line1, line2, line3
+        
+anim = animation.FuncAnimation(fig, animate, np.arange(0,9), init_func=init)
+
 plt.show()
+
+## plot contours
+#plt.figure()
+#plt.gca().invert_yaxis()
+#
+#for i in range(1,9):
+#
+#    # load result
+#    result = np.loadtxt(data_dir + "affine" + str(i) + ".txt")
+#
+#    # plot
+#    plt.clf()
+#    plt.plot(c1[:,0], c1[:,1], 'b-')
+#    plt.plot(c2[:,0], c2[:,1], 'ro')
+#    plt.plot(result[:,0], result[:,1], 'go')
+#    
+#    # pause briefly
+#    plt.pause(0.5)
--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,27 @@
 
 using namespace std;
 
+template <class T>
+void save_to_file(string filename, T result)
+{
+    ofstream file(filename);
+    file << result.points;
+    file.close();
+}
+
+string result_filename(string registration_type, int iteration)
+{
+    return registration_type + to_string(iteration) + ".txt";
+}
 
 int
 main (int argc, char** argv)
 {
-    cout << "Starting CPD Example ..." << endl;
+    cout << "Starting CPD Demo ..." << endl;
 
     // set data directory
-    string data_dir = "/home/damon/gcubed/repos/cpdexample/";
+    string data_dir = "/home/damon/gcubed/repos/cpdexample/data/";
+    int max_iterations = 10;
 
     // load contour 1
     cout << "loading contour 1" << endl;
@@ -27,12 +40,33 @@ main (int argc, char** argv)
     string fname2 = data_dir + "contour2.txt";
     cpd::Matrix mat2 = cpd::matrix_from_path(fname2);
 
-    // perform registration
-    //auto result = cpd::nonrigid(mat1, mat2);
-    auto result = cpd::affine(mat1, mat2);
+    // perform registrations
+    //auto nonrigid_result = cpd::nonrigid(mat1, mat2);
+    //auto affine_result = cpd::affine(mat1, mat2);
 
-    // save result to text file
-    ofstream result_file(data_dir + "result.txt");
-    result_file << result.points;
-    result_file.close();
+    cpd::Affine affine;
+    cpd::Nonrigid nonrigid;
+    cpd::RigidResult affine_result;
+    cpd::NonrigidResult nonrigid_result;
+
+    for(int iteration = 1; iteration < max_iterations; ++iteration)
+    {
+        // set max iterations
+        affine.set_max_iterations(iteration);
+        nonrigid.set_max_iterations(iteration);
+
+        // perform registration
+        affine_result = affine.compute(mat1, mat2);
+        nonrigid_result = nonrigid.compute(mat1, mat2);
+
+        // save registration results
+        {
+            save_to_file(data_dir + result_filename("nonrigid", iteration), nonrigid_result);
+            save_to_file(data_dir + result_filename("affine",   iteration), affine_result);
+        }
+
+    }
+
+
+
 }
