f5f931188d163abba537af98e6df048bcb12fec0
Damon Kelly <damonkelly@gmail.com>
2016-07-20 20:34:46 -0400

    Show both affine and nonrigid results

--- scripts/plotresult.py
+++ scripts/plotresult.py
@@ -18,27 +18,48 @@ c2 = np.loadtxt(data_dir + "contour2.txt")
 
 
 # Setup up the figure, axis and plot elements to animate
-fig, ax = plt.subplots()
+fig = plt.figure()
+
+# affine subplot
+plt.subplot(121)
 plt.title("affine cpd")
 plt.gca().invert_yaxis()
-line1, = ax.plot(c1[:,0], c1[:,1], 'b+')    # fixed target
-line2, = ax.plot([], [], 'go')              # src points
+line11, = plt.plot(c1[:,0], c1[:,1], 'b+')    # fixed target
+line12, = plt.plot([], [], 'go')              # src points
+
+# nonrigid subplot
+plt.subplot(122)
+plt.title("nonrigid cpd")
+plt.gca().invert_yaxis()
+line21, = plt.plot(c1[:,0], c1[:,1], 'b+')    # fixed target
+line22, = plt.plot([], [], 'go')              # src points
+
+
+#fig, ax = plt.subplots()
+#plt.title("affine cpd")
+#plt.gca().invert_yaxis()
+#line1, = ax.plot(c1[:,0], c1[:,1], 'b+')    # fixed target
+#line2, = ax.plot([], [], 'go')              # src points
 
 
 # Initialization function plots the background of each frame
 def init():
-    line1.set_data(c1[:,0], c1[:,1])    # fixed target
-    return line1, line2, line3
+    line11.set_data(c1[:,0], c1[:,1])    # fixed target
+    line21.set_data(c1[:,0], c1[:,1])    # fixed target
+    return line11, line12
 
 # Animation function which is called sequentially
 def animate(i):
     print(i)
     if i == 0:
-        line2.set_data(c2[:,0], c2[:,1])
+        line12.set_data(c2[:,0], c2[:,1])
+        line22.set_data(c2[:,0], c2[:,1])
     else:
-        result = np.loadtxt(data_dir + "affine" + str(i) + ".txt")
-        line2.set_data(result[:,0], result[:,1])
-    return line1, line2, line3
+        result1 = np.loadtxt(data_dir + "affine" + str(i) + ".txt")
+        result2 = np.loadtxt(data_dir + "nonrigid" + str(i) + ".txt")
+        line12.set_data(result1[:,0], result1[:,1])
+        line22.set_data(result2[:,0], result2[:,1])
+    return line11, line22
         
 anim = animation.FuncAnimation(fig, animate, np.arange(0,9), init_func=init)
 
